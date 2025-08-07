5858563526c66dc0c28a9825b31d3fb4a69621eb
princelulinda <princelulinda@gmail.com>
2025-08-07 03:06:38 +0200

    add host allowed

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,6 +3,16 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    vite: () => {
+      return {
+        server: {
+          allowedHosts: ["trustt.duckdns.org"],
+        },
+      }
+    },
+  },
+
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
